368189fed92455720d4eb0c55a8866448d2b1774
Alexander Shelemetyev <avs.kudri@gmail.com>
2019-05-29 09:08:56 +0300

    change defaults

--- smsgorod.py
+++ smsgorod.py
@@ -8,7 +8,7 @@ class Gateweay(object):
 
     def __init__(self, account):
         self.key = account.get('key','')
-        self.sender = account.get('sender','UNO')
+        self.sender = account.get('sender','UNO.salon')
 
     def params(self):
         return {'apiKey': self.key}
