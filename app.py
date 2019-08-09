06ad9f20c6b73d29404358768c09f4ee402a59b7
Alexander Shelemetyev <ex.lxndr@gmail.com>
2019-08-09 07:08:58 +0300

    mod sms price

--- app.py
+++ app.py
@@ -22,168 +22,195 @@ app.logger.handlers.extend(gunicorn_error_logger.handlers)
 app.logger.setLevel(logging.DEBUG)
 
 DEFAULT_SMS_CONFIG = {
-            'provider': {
-                'name': os.getenv('SMS_DEFAULT_PROVIDER'),
-                'key': os.getenv('SMS_DEFAULT_PROVIDER_KEY'), 
-		            'sender': os.getenv('SMS_DEFAULT_PROVIDER_SENDER')
-            },
-            'price': float(os.getenv('SMS_DEFAULT_PRICE'))
-          }
+    'provider': {
+        'name': os.getenv('SMS_DEFAULT_PROVIDER'),
+        'key': os.getenv('SMS_DEFAULT_PROVIDER_KEY'),
+        'sender': os.getenv('SMS_DEFAULT_PROVIDER_SENDER')
+    },
+    'price': float(os.getenv('SMS_DEFAULT_PRICE'))
+}
+
 
 def provider(providerName):
-  providers = {
-    'Stream Telecom': streamtel.Gateway,
-    'SMS gorod': smsgorod.Gateway,
-    'Beeline': beeline.Gateway
-  }
-  print('provider', providers[providerName])
-  return providers[providerName]
+    providers = {
+        'Stream Telecom': streamtel.Gateway,
+        'SMS gorod': smsgorod.Gateway,
+        'Beeline': beeline.Gateway
+    }
+    print('provider', providers[providerName])
+    return providers[providerName]
 
 
 def valid_uuid(uuid):
-    regex = re.compile('^[a-f0-9]{8}-?[a-f0-9]{4}-?[a-f0-9]{4}-?[a-f0-9]{4}-?[a-f0-9]{12}\Z', re.I)
+    regex = re.compile(
+        '^[a-f0-9]{8}-?[a-f0-9]{4}-?[a-f0-9]{4}-?[a-f0-9]{4}-?[a-f0-9]{12}\Z', re.I)
     match = regex.match(uuid)
     return bool(match)
 
 
 def getDBAccountInfo(business_id):
-  if business_id == None:
-    return {'business_id': None}
-  if not valid_uuid(business_id):
-    app.logger.debug(f"Bad business ID: {business_id}")
-    raise ValueError("Bad business ID")
-  notification_settings=DEFAULT_SMS_CONFIG
-  account=[{'business_id': business_id, 'notification_settings': DEFAULT_SMS_CONFIG}]
-  return account[0]
+    if business_id == None:
+        return {'business_id': None}
+    if not valid_uuid(business_id):
+        app.logger.debug(f"Bad business ID: {business_id}")
+        raise ValueError("Bad business ID")
+    notification_settings = DEFAULT_SMS_CONFIG
+    account = [{'business_id': business_id,
+                'notification_settings': DEFAULT_SMS_CONFIG}]
+    return account[0]
 
 
 def getGateway(business_id):
-  app.logger.info(f'getGateway: {business_id}')
-  if not business_id == None and not valid_uuid(business_id):
-    app.logger.debug(f"Bad business ID: {business_id}")
-    raise ValueError("Bad business ID")
-  if business_id == None:
-    notification_settings=DEFAULT_SMS_CONFIG
-    account = {'business_id': None}
-  else:
-    account = getDBAccountInfo(business_id)
-    if account == None:
-      app.logger.debug("Business account not found")
-      raise ValueError("Business account not found")
-    notification_settings = account.get('notification_settings')
-  if notification_settings == None:
-    app.logger.debug("Notification settings not found")
-    raise ValueError("Notification settings not found")
-  providerInfo = notification_settings.get('provider')
-  gateway = provider(providerInfo.get('name'))(providerInfo)
-  app.logger.info(f'getGateway {gateway}, {account}')
-  return gateway, account
+    app.logger.info(f'getGateway: {business_id}')
+    if not business_id == None and not valid_uuid(business_id):
+        app.logger.debug(f"Bad business ID: {business_id}")
+        raise ValueError("Bad business ID")
+    if business_id == None:
+        notification_settings = DEFAULT_SMS_CONFIG
+        account = {'business_id': None}
+    else:
+        account = getDBAccountInfo(business_id)
+        if account == None:
+            app.logger.debug("Business account not found")
+            raise ValueError("Business account not found")
+        notification_settings = account.get('notification_settings')
+    if notification_settings == None:
+        app.logger.debug("Notification settings not found")
+        raise ValueError("Notification settings not found")
+    providerInfo = notification_settings.get('provider')
+    gateway = provider(providerInfo.get('name'))(providerInfo)
+    app.logger.info(f'getGateway {gateway}, {account}')
+    return gateway, account
 
 
 @app.route('/send')
 def send_message():
-  print('send_message: ', request.args)
-  app.logger.info(f'IN: {request.args}')
-  amount = float(DEFAULT_SMS_CONFIG.get('price', 5.0))
-  phone = request.args.get('phone')
-  if phone == None:
-    app.logger.debug("No phone")
-    return json.dumps({'error': 'No phone'}) 
-  text = request.args.get('text')
-  if text == None:
-    app.logger.debug("No text")
-    return json.dumps({'error': 'No text'})
-  business_id = request.args.get('business_id', '')
-  if business_id == '':
-    business_id = None
-  time = request.args.get('time', '')
+    print('send_message: ', request.args)
+    app.logger.info(f'IN: {request.args}')
+    amount = float(DEFAULT_SMS_CONFIG.get('price', 5.0))
+    phone = request.args.get('phone')
+    if phone is None:
+        app.logger.debug("No phone")
+        return json.dumps({'error': 'No phone'})
+    text = request.args.get('text')
+    if text is None:
+        app.logger.debug("No text")
+        return json.dumps({'error': 'No text'})
+    business_id = request.args.get('business_id', '')
+    if business_id == '':
+        business_id = None
+    time = request.args.get('time', '')
 
-  try:
-    gateway, account = getGateway(business_id)
-  except Exception as e:
-    app.logger.error("error: {0}".format(e))
-    return json.dumps({'error': "{0}".format(e)}) 
+    try:
+        gateway, account = getGateway(business_id)
+    except Exception as e:
+        app.logger.error("error: {0}".format(e))
+        return json.dumps({'error': "{0}".format(e)})
 
-  transaction_id = None
-  transaction_result = None
+    transaction_id = None
+    transaction_result = None
 
-  if not business_id is None:
-    # пытаемся зарезервировать сумму на счете
-    transaction_id, transaction_result = billing.SMSReserveSum(business=business_id, amount=amount, params={})
-    app.logger.debug(f'Transaction: {transaction_id}, {transaction_result}')
-    if transaction_id == None:
-      return json.dumps(transaction_result or {'error', 'Transaction failed'})
+    if business_id is not None:
+        # пытаемся зарезервировать сумму на счете
+        transaction_id, transaction_result = billing.SMSReserveSum(
+            business=business_id, amount=amount, params={})
+        app.logger.debug(
+            f'Transaction: {transaction_id}, {transaction_result}')
+        if transaction_id is None:
+            return json.dumps(transaction_result or {'error',
+                                                     'Transaction failed'})
 
-  # оправляем СМС
-  res = gateway.send(phone, text, time)
-  app.logger.debug(f"Response: {res}")
+    # оправляем СМС
+    res = gateway.send(phone, text, time)
+    app.logger.debug(f"Response: {res}")
 
-  if not transaction_id is None:
-    # Отменяем резервирование
-    # TODO указать причину отмены
-    transaction_id, transaction_result = billing.undoTransaction(transactionId=transaction_id, params={'gatewayResponse': res, 'provider': DEFAULT_SMS_CONFIG.get('provider',{}).get('name')})
+    if transaction_id is not None:
+        # Отменяем резервирование
+        # TODO указать причину отмены
+        transaction_id, transaction_result =
+        billing.undoTransaction(transactionId=transaction_id, params={
+            'gatewayResponse': res,
+            'provider': DEFAULT_SMS_CONFIG.get('provider', {}).get('name')})
 
-  if res.get('success', False):
-    # опять резервируем, но уже с ID провайдера СМС, чтобы потом проверить статус СМС
-    transaction_id, transaction_result = billing.SMSReserveSum(business=business_id, amount=amount, params={'gatewayResponse': res, 'provider': DEFAULT_SMS_CONFIG.get('provider',{}).get('name')})
+    if res.get('success', False):
+        # опять резервируем, но уже с ID провайдера СМС,
+        # чтобы потом проверить статус СМС
+        parts = gateway.Parts(res)
+        if parts not is None:
+            amount = amount * parts
+        transaction_id, transaction_result =
+        billing.SMSReserveSum(business=business_id, amount=amount, params={
+            'gatewayResponse': res,
+            'provider': DEFAULT_SMS_CONFIG.get('provider', {}).get('name')})
 
-  try:
-    return json.dumps({'response': res, 'transaction': transaction_id})
-  except Exception as e:
-    return json.dumps({'error': "{0}".format(e)}) 
+    try:
+        return json.dumps({'response': res, 'transaction': transaction_id})
+    except Exception as e:
+        return json.dumps({'error': "{0}".format(e)})
+
 
 @app.route('/check', methods=['POST'])
 def check_messages():
-  
-  data = request.get_json()
 
-  try:
-    gateway, account = getGateway(None)
-  except Exception as e:
-    app.logger.error("error: {0}".format(e))
-    return json.dumps({'error': "{0}".format(e)}) 
+    data = request.get_json()
 
-  transactions = billing.getWaitingTransactions()
+    try:
+        gateway, account = getGateway(None)
+    except Exception as e:
+        app.logger.error("error: {0}".format(e))
+        return json.dumps({'error': "{0}".format(e)})
 
-  app.logger.debug(f"transactions: {transactions}")
-  IDs = [item.get('id') for item in transactions]
+    transactions = billing.getWaitingTransactions()
 
-  if len(IDs) == 0:
-     return json.dumps({'result': 'no data'}) 
+    app.logger.debug(f"transactions: {transactions}")
+    IDs = [item.get('id') for item in transactions]
 
-  res = gateway.status(IDs)
-  app.logger.debug(f"Message statuses: {res}")
+    if len(IDs) == 0:
+        return json.dumps({'result': 'no data'})
 
-  if res.get('status') == 'error':
-    return json.dumps({'response': res})
+    res = gateway.status(IDs)
+    app.logger.debug(f"Message statuses: {res}")
 
-  costs = [item for item in res.get('data')]
+    if res.get('status') == 'error':
+        return json.dumps({'response': res})
 
-  app.logger.debug(f"costs: {costs}")
+    costs = [item for item in res.get('data')]
 
-  for sms in costs:
-    foundTransactions = [item for item in transactions if str(item.get('id'))==str(sms.get('id'))]
-    if len(foundTransactions) != 1:
-      app.logger.debug(f"Wrong SMS ID: {sms}")
-      continue
+    app.logger.debug(f"costs: {costs}")
 
-    if sms.get('status') == 'delivered': 
-      transaction_id, transaction_result = billing.SMSDelivered(business=foundTransactions[0].get('business'), amount=float(sms.get('cost')), transactionId=foundTransactions[0].get('transaction'), params={'smsStatus': sms, 'provider': foundTransactions[0].get('provider')})
+    for sms in costs:
+        foundTransactions = [item for item in transactions if str(
+            item.get('id')) == str(sms.get('id'))]
+        if len(foundTransactions) != 1:
+            app.logger.debug(f"Wrong SMS ID: {sms}")
+            continue
 
-  try:
-    return json.dumps({'response': res})
-  except Exception as e:
-    return json.dumps({'error': "{0}".format(e)}) 
+        if sms.get('status') == 'delivered':
+            transaction_id,
+            transaction_result = billing.SMSDelivered(
+                business=foundTransactions[0].get('business'),
+                amount=float(sms.get('cost')),
+                transactionId=foundTransactions[0].get('transaction'),
+                params={
+                    'smsStatus': sms,
+                    'provider': foundTransactions[0].get('provider')
+                })
+
+    try:
+        return json.dumps({'response': res})
+    except Exception as e:
+        return json.dumps({'error': "{0}".format(e)})
 
 
 @app.route('/balance/<business_id>', methods=['GET'])
 def balance(business_id):
-  try:
-    gateway = getGateway(business_id)
-    res = gateway.balance()
-    return json.dumps({'response': res})
-  except Exception as e:
-    return json.dumps({'error': "{0}".format(e)}) 
+    try:
+        gateway = getGateway(business_id)
+        res = gateway.balance()
+        return json.dumps({'response': res})
+    except Exception as e:
+        return json.dumps({'error': "{0}".format(e)})
+
 
 if __name__ == "__main__":
 
--- smsgorod.py
+++ smsgorod.py
@@ -2,46 +2,48 @@ from singleton import Singleton
 from requests import get, post
 import json
 
+
 class Gateway(object):
 
     _apiGateway = 'https://new.smsgorod.ru/apiSms/'
 
     def __init__(self, account):
-        self.key = account.get('key','')
-        self.sender = account.get('sender','UNO.salon')
+        self.key = account.get('key', '')
+        self.sender = account.get('sender', 'UNO.salon')
 
     def params(self):
         return {'apiKey': self.key}
 
-    def send(self, destination, text, time = None):
+    def send(self, destination, text, time=None):
         if self.key is None or self.key == '':
             return {'success': False, 'details': 'No API key'}
 
         params = self.params()
         sms = {
-                    "channel": "char",
-                    "phone": destination,
-                    "text": text,
-                    "sender": self.sender
-                }
+            "channel": "char",
+            "phone": destination,
+            "text": text,
+            "sender": self.sender
+        }
         if time != None and time != '':
             sms.update({"plannedAt": time})
-        params.update({'sms': 
-            [
-                sms
-            ]
-        })
+        params.update({'sms':
+                       [
+                           sms
+                       ]
+                       })
 
         print('Params: ', params)
         headers = {
-            'Content-type': 'application/json', 
+            'Content-type': 'application/json',
             'accept': 'application/json'
         }
         res = post(self._apiGateway + 'create', json=params, headers=headers)
         print('POST: ', res, res.status_code)
         print(res.url, res.text)
-        response=res.json()
-        status=response.get('status','error') != 'error' and response.get('status','error') != 'channel'
+        response = res.json()
+        status = response.get('status', 'error') != 'error' and response.get(
+            'status', 'error') != 'channel'
         return {'success': status, 'response': response}
 
     def status(self, IDs):
@@ -50,7 +52,7 @@ class Gateway(object):
         params = self.params()
         params['apiSmsIdList'] = IDs
         headers = {
-            'Content-type': 'application/json', 
+            'Content-type': 'application/json',
             'accept': 'application/json'
         }
         res = post(self._apiGateway + 'get', json=params, headers=headers)
@@ -61,17 +63,28 @@ class Gateway(object):
             return {'success': False, 'details': 'No API key'}
         params = self.params()
         headers = {
-            'Content-type': 'application/json', 
+            'Content-type': 'application/json',
             'accept': 'application/json'
         }
-        res = get(self._apiGateway + 'getSmsList', params=params, headers=headers)
+        res = get(self._apiGateway + 'getSmsList',
+                  params=params, headers=headers)
         return res.json()
-
 
     def balance(self):
         pass
 
-
-        
-
-
+    def Parts(response):
+        #  {"success": true, "response": {"data": [
+        #       {"id": 340142999, "cost": 0, "phone": "79537680916",
+        #           "sender": "UNO.salon", "source": "api",
+        #           "status": "sent", "channel": "char",
+        #           "message": "Новая онлайн запись к Сотрудник сотрудникович
+        #                   на 09.08 11:00 в Тестовый Филиал компании",
+        #           "smsCount": 2, "createdAt": 1565319610,
+        #           "updatedAt": 1565319610
+        #       }
+        # ], "status": "success"}}
+        if response is None:
+            return
+        return response.get('response', {}).get('data', []).\
+            get(0, {}).get('smsCount', None)
