986ecfde21723e7b68f692a8234f1e9a5c6309c0
Alexander Shelemetyev <ex.lxndr@gmail.com>
2019-08-09 08:07:15 +0300

    fix lint error

--- smsgorod.py
+++ smsgorod.py
@@ -86,6 +86,7 @@ class Gateway(object):
         # ], "status": "success"}}
 
         try:
-            return float(response.get('response', {}).get('data', [])[0].get('smsCount', None))
+            return float(response.get('response', {})\
+                .get('data', [])[0].get('smsCount', None))
         except:
             return None
